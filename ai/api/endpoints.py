33e352ab5f8840015f4235a6eab17f58341b1f7d
PRAGNA <eduardermakov769@gmail.com>
2025-11-05 00:55:41 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        SHADIX
+import os        PRAGNA
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
