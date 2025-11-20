93b30e3a2aaf003694238263311d5c9c9b6cdd36
PAXINE <eduardermakov769@gmail.com>
2025-11-20 21:50:02 +0800

    Update eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,4 +1,4 @@
-import js from '@eslint/js';   HAUNIX
+import js from '@eslint/js';   PAXINE
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks'; 
 import reactRefresh from 'eslint-plugin-react-refresh';
