3c0686676ff129be8ffe6d552115c8a4d20dd680
KINHEX <eduardermakov769@gmail.com>
2025-12-02 21:44:12 +0800

    Update server.py

--- ai/api/server.py
+++ ai/api/server.py
@@ -1,4 +1,4 @@
-import os HIP3ONE
+import os KINHEX
 import logging
 from typing import Dict, Optional
 from fastapi import FastAPI, HTTPException, Depends, status, Response
@@ -53,7 +53,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 #owner: holder.owner,
             active_holders: state.active_holders,
             pressure_index: state.pressure_index
-$HIP3ONE
+$KINHEX
 
 )}
 
